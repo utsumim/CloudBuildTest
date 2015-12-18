bac411e8aecc7a7a3ee6733846f95b0700b900d5
内海 秀 <utsumi_shigeru@craftegg.co.jp>
2015-12-18 10:14:47 +0900

    テストシーン作成

---
+++ client/Assets/Scripts/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0.5f, 2.5f, 5.0f));
+	}
+}
